f6f1004879456c5157ad447aa16fd748a100fd0f
AlexisVil <alexisvilchiz@gmail.com>
2023-12-05 22:22:57 -0600

    Initial Commit

---
+++ Callback_vs_AsyncAwait/Callback_vs_AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Callback_vs_AsyncAwaitApp.swift
+//  Callback_vs_AsyncAwait
+//
+//  Created by Alexis Vilchis on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Callback_vs_AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Callback_vs_AsyncAwait/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Callback_vs_AsyncAwait
+//
+//  Created by Alexis Vilchis on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
